0edf8bf925fcaa4c9487e7aaa7cf8fb7233d2fa2
Abhima9523 <abhimanyu2u2001@gmail.com>
2024-08-30 13:57:27 -0700

    port

---
+++ src/components/About.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+          Hello, I'm Abhimanyu, a passionate Web developer with a keen eye for FULLSTACK
+          . With a background in IT, I strive to create impactful and
+          visually stunning Software solutions that leave a lasting impression.
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education & Training
+        </h1>
+        <span>
+          [B.tech], [Electrical And Electronic], [2023] [Intermidiate],
+          [Math], [2019] [Fullstack developer], [Jspider BTM],
+          [2023]
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span>
+          Proficient in [Programming Languages] Experienced with [Software
+          Tools/Technologies] Strong grasp of [Design Principles/Concepts]
+          Excellent Dsa problem-solving skills Effective communicator and
+          collaborator
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills
+        </h1>
+        <span>
+          [Core Java], [Html], [Css] [JavaScript] [Job Title],
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Frameworks
+        </h1>
+        <span>
+          [Jdbc], [Hibernate], [Servlet], [Spring], [Springboot],
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Libraries
+        </h1>
+        <span>
+          [React], [Tailwinds], [Springboot], [Sql],
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Achievements & Awards
+        </h1>
+        <span>
+          [Cartificate], [Hacker Rank], [2023] [Achievement],
+          [Organization/Platform], [Year]
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+          My mission is to leverage my skills and creativity to deliver
+          innovative [Your Field] solutions that exceed client expectations and
+          contribute positively to the digital landscape. I am committed to
+          continuous learning and growth, always seeking new challenges and
+          opportunities to expand my horizons.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Home.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+
+import pic from "../../public/mainpng1.jpg";
+
+import { FaLinkedin } from "react-icons/fa";
+import { BsGithub } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
+import { FaTelegram } from "react-icons/fa6";
+
+import { FaReact } from "react-icons/fa";
+import { RiJavaLine } from "react-icons/ri";
+import { IoLogoJavascript } from "react-icons/io";
+import { SiSpringboot } from "react-icons/si";
+import { FaNodeJs } from "react-icons/fa6";
+
+import { ReactTyped } from "react-typed";
+
+
+function Home() {
+  return (
+    <>
+      <div
+        name="Home"
+        className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-28"
+      >
+        <div className="flex flex-col md:flex-row">
+          <div className="md:w-1/2 mt-12 md:mt-24 space-y-2 order-2 md:order-1">
+            <span className="text-xl">Welcome In My Portfolio</span>
+            <div className="flex space-x-1 text-2xl md:text-4xl">
+              <h1>Hello, I'm a</h1>
+              {/* <span >Developer</span> */}
+              <ReactTyped
+                className="text-red-700 font-bold"
+                strings={["Developer", "Programmer", "Coder"]}
+                typeSpeed={40}
+                backSpeed={50}
+                loop={true}
+              />
+            </div>
+            <br />
+            <p className="text-sm md:text-md text-justify">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Quisquam, recusandae consequatur. Accusamus sint libero eligendi
+              laborum fuga repudiandae? Asperiores tempore eos animi dolores
+              corrupti! Sint quos, mollitia architecto aliquam nesciunt, optio
+              cum delectus sit voluptatum aut ratione quaerat veniam aperiam!
+            </p>
+            <br />
+            {/* social media icons */}
+            <div className="flex flex-col items-center md:flex-row justify-between space-y-6 md:space-y-0">
+              <div className="  space-y-2">
+                <h1 className="font-bold text-center ">Available on</h1>
+                <ul className="flex space-x-5">
+                  <li>
+                  <a href="http://www.linkedin.com/in/abhimanyu-kumar9818" target="_blank">
+                      <FaLinkedin className="text-2xl cursor-pointer" />
+                    </a>
+                  </li>
+                  <li>
+                  <a href="https://github.com/Abhima9523/Ahimanyu-kumar/" target="_blank">
+                  <BsGithub  className="text-2xl cursor-pointer" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="mailto:abhimanyukr9818@gmail.com" target="_blank">
+                    <MdEmail className="text-2xl cursor-pointer" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://github.com/Abhima9523/Ahimanyu-kumar/" target="_blank">
+                      <FaTelegram className="text-2xl cursor-pointer" />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className=" space-y-2">
+                <h1 className="font-bold text-center">Currently working on</h1>
+                <div className="flex space-x-5">
+                <FaReact  className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer" />
+                <RiJavaLine className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer" />
+                <IoLogoJavascript  className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer" />
+                  <FaNodeJs className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer" />
+                  <SiSpringboot  className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer"/>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="md:w-1/2 md:ml-48 md:mt-20 mt-8 order-1">
+            <img
+              src={pic}
+              className=" md:w-[450px] md:h-[500px] shadow-lg mt-[-20px]"
+              alt=""
+              // rounded-full
+            />
+          </div>
+        </div>
+      </div>
+
+      <hr />
+    </>
+  );
+}
+
+export default Home;
